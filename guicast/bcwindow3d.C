1b19d6f3f704d4d3b5f25c4ea5e708b3d0dff8c6
Einar Rünkaru <einarry@smail.ee>
2010-04-06 19:07:13 +0300

    Check OpenGL server version at runtime.
    According to http://www.opengl.org/sdk/docs/man/xhtml/glXCreatePbuffer.xml
    "glXCreatePbuffer is available only if the GLX version is 1.3 or greater."
    Note that this is about GLX version. This means, that on the client side
    a version of OpenGL greater than 1.3 can be installed (and in fact must be
    otherwise, Cinelerra would not even start), but on the server side the
    GLX version can still be lower.
    
    If OpenGL version is less than 1.3 don't show OpenGl selection in
    preferences..playback.

--- guicast/bcwindow3d.C
+++ guicast/bcwindow3d.C
@@ -98,12 +98,12 @@ void BC_WindowBase::put_shader(unsigned int handle, char *source)
 	get_resources()->get_synchronous()->put_shader(handle, source);
 }
 
-
-
-
-
-
-
-
-
-
+int BC_WindowBase::get_opengl_server_version()
+{
+#ifdef HAVE_GL
+	int maj, min;
+	if(glXQueryVersion(get_display(), &maj, &min))
+		return 100 * maj + min;
+#endif
+	return 0;
+}
