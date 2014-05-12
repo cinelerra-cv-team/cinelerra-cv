68c519d2b96110e8dae15b8a9943472b087dbea1
Petter Reinholdtsen <pere@hungry.com>
2014-05-12 22:51:54 +0200

    Change 'char *' to 'const char *' in XMLTag class to get rid of warnings.
    This reduce the amount of warnings and bring the code closer to HV 4.5.

--- cinelerra/filexml.C
+++ cinelerra/filexml.C
@@ -89,13 +89,13 @@ int FileXML::append_tag()
 	return 0;
 }
 
-int FileXML::append_text(char *text)
+int FileXML::append_text(const char *text)
 {
 	append_text(text, strlen(text));
 	return 0;
 }
 
-int FileXML::append_text(char *text, long len)
+int FileXML::append_text(const char *text, long len)
 {
 	while(position + len > available)
 	{
@@ -109,7 +109,7 @@ int FileXML::append_text(char *text, long len)
 	return 0;
 }
 
-int FileXML::encode_text(char *text)
+int FileXML::encode_text(const char *text)
 {
 // We have to encode at least the '<' char
 // We encode three things:
@@ -230,7 +230,7 @@ int FileXML::read_tag()
 	return tag.read_tag(string, position, length);
 }
 
-int FileXML::read_text_until(char *tag_end, char *output, int max_len)
+int FileXML::read_text_until(const char *tag_end, char *output, int max_len)
 {
 // read to next tag
 	int out_position = 0;
@@ -275,7 +275,7 @@ int FileXML::read_text_until(char *tag_end, char *output, int max_len)
 }
 
 
-int FileXML::write_to_file(char *filename)
+int FileXML::write_to_file(const char *filename)
 {
 	FILE *out;
 	strcpy(this->filename, filename);
@@ -321,7 +321,7 @@ int FileXML::write_to_file(FILE *file)
 	return 0;
 }
 
-int FileXML::read_from_file(char *filename, int ignore_error)
+int FileXML::read_from_file(const char *filename, int ignore_error)
 {
 	FILE *in;
 	
@@ -573,7 +573,7 @@ int XMLTag::read_tag(char *input, long &position, long length)
 	return 0;
 }
 
-int XMLTag::title_is(char *title)
+int XMLTag::title_is(const char *title)
 {
 	if(!strcasecmp(title, tag_title)) return 1;
 	else return 0;
@@ -603,7 +603,7 @@ int XMLTag::test_property(char *property, char *value)
 	return 0;
 }
 
-char* XMLTag::get_property(char *property, char *value)
+char* XMLTag::get_property(const char *property, char *value)
 {
 	int i, result;
 	for(i = 0, result = 0; i < total_properties && !result; i++)
@@ -628,7 +628,7 @@ char* XMLTag::get_property(char *property, char *value)
 	return value;
 }
 
-char* XMLTag::get_property_text(int number)
+const char* XMLTag::get_property_text(int number)
 {
 	if(number < total_properties) 
 		return tag_properties[number];
@@ -652,7 +652,7 @@ float XMLTag::get_property_float(int number)
 		return 0;
 }
 
-char* XMLTag::get_property(char *property)
+char* XMLTag::get_property(const char *property)
 {
 	int i, result;
 	for(i = 0, result = 0; i < total_properties && !result; i++)
@@ -666,7 +666,7 @@ char* XMLTag::get_property(char *property)
 }
 
 
-int32_t XMLTag::get_property(char *property, int32_t default_)
+int32_t XMLTag::get_property(const char *property, int32_t default_)
 {
 	temp_string[0] = 0;
 	get_property(property, temp_string);
@@ -676,7 +676,7 @@ int32_t XMLTag::get_property(char *property, int32_t default_)
 		return atol(temp_string);
 }
 
-int64_t XMLTag::get_property(char *property, int64_t default_)
+int64_t XMLTag::get_property(const char *property, int64_t default_)
 {
 	int64_t result;
 	temp_string[0] = 0;
@@ -690,7 +690,7 @@ int64_t XMLTag::get_property(char *property, int64_t default_)
 	return result;
 }
 // 
-// int XMLTag::get_property(char *property, int default_)
+// int XMLTag::get_property(const char *property, int default_)
 // {
 // 	temp_string[0] = 0;
 // 	get_property(property, temp_string);
@@ -698,7 +698,7 @@ int64_t XMLTag::get_property(char *property, int64_t default_)
 // 	else return atol(temp_string);
 // }
 // 
-float XMLTag::get_property(char *property, float default_)
+float XMLTag::get_property(const char *property, float default_)
 {
 	temp_string[0] = 0;
 	get_property(property, temp_string);
@@ -708,7 +708,7 @@ float XMLTag::get_property(char *property, float default_)
 		return atof(temp_string);
 }
 
-double XMLTag::get_property(char *property, double default_)
+double XMLTag::get_property(const char *property, double default_)
 {
 	temp_string[0] = 0;
 	get_property(property, temp_string);
@@ -718,27 +718,27 @@ double XMLTag::get_property(char *property, double default_)
 		return atof(temp_string);
 }
 
-int XMLTag::set_title(char *text)       // set the title field
+int XMLTag::set_title(const char *text)       // set the title field
 {
 	strcpy(tag_title, text);
 	return 0;
 }
 
-int XMLTag::set_property(char *text, int32_t value)
+int XMLTag::set_property(const char *text, int32_t value)
 {
 	sprintf(temp_string, "%ld", value);
 	set_property(text, temp_string);
 	return 0;
 }
 
-int XMLTag::set_property(char *text, int64_t value)
+int XMLTag::set_property(const char *text, int64_t value)
 {
 	sprintf(temp_string, "%lld", value);
 	set_property(text, temp_string);
 	return 0;
 }
 
-int XMLTag::set_property(char *text, float value)
+int XMLTag::set_property(const char *text, float value)
 {
 	if (value - (float)((int64_t)value) == 0)
 		sprintf(temp_string, "%lld", (int64_t)value);
@@ -748,7 +748,7 @@ int XMLTag::set_property(char *text, float value)
 	return 0;
 }
 
-int XMLTag::set_property(char *text, double value)
+int XMLTag::set_property(const char *text, double value)
 {
 	if (value - (double)((int64_t)value) == 0)
 		sprintf(temp_string, "%lld", (int64_t)value);
@@ -758,7 +758,7 @@ int XMLTag::set_property(char *text, double value)
 	return 0;
 }
 
-int XMLTag::set_property(char *text, char *value)
+int XMLTag::set_property(const char *text, const char *value)
 {
 	tag_properties[total_properties] = new char[strlen(text) + 1];
 	strcpy(tag_properties[total_properties], text);
