cinelerra-cv-team/cinelerra-cv

commit 6d6012573eb8176d76329a7fbcc847b66879e587

Enable UTF-8 output and input in BC_WindowBase
Remove "UTF-8 dirty hack" and then enabled Xmbdrawstring.
Update all the XFT subsystem functions, on the way to managing UTF8.
UTF-8 input is treated by bcwindowbase with the new function get_keypress_utf8().

Thanks to Garett Wilson, Raffaella Trainello and Herman Robak for useful help.

Committer changed formatting, rearranged commits.

guicast/bcwindowbase.C
@@ -675,7 +675,11 @@ int BC_WindowBase::dispatch_event()
 	XEvent *event = 0;
     Window tempwin;
   	KeySym keysym;
+#ifdef X_HAVE_UTF8_STRING
+	char keys_return[6];
+#else
   	char keys_return[2];
+#endif
 	int result;
 	XClientMessageEvent *ptr;
 	int temp;
@@ -884,22 +888,75 @@ int BC_WindowBase::dispatch_event()
 
 		case KeyPress:
 			get_key_masks(event);
-  			keys_return[0] = 0;
-  			XLookupString((XKeyEvent*)event, keys_return, 1, &keysym, 0);
+#ifdef X_HAVE_UTF8_STRING
+			memset(keys_return, 0, sizeof(keys_return));
+			// this is routine re-adapted from xev.c - xutils
+			im = XOpenIM (display, NULL, NULL, NULL);
+			XIMStyles *xim_styles;
+			XIMStyle xim_style;
 
+			if (im == NULL)
+				printf ("XOpenIM failed\n");
+
+			if (im)
+			{
+				char *imvalret;
+				imvalret = XGetIMValues (im, XNQueryInputStyle, &xim_styles, NULL);
+				if (imvalret != NULL || xim_styles == NULL) 
+					printf ("input method doesn't support any styles\n");
+
+				if (xim_styles)
+				{
+					xim_style = 0;
+					for (int z = 0;  z < xim_styles->count_styles;  z++) 
+					{
+						if (xim_styles->supported_styles[z] == (XIMPreeditNothing | XIMStatusNothing)) 
+						{
+							xim_style = xim_styles->supported_styles[z];
+							break;
+						}
+					}
+
+					if (xim_style == 0)
+						printf ("input method doesn't support the style we support\n");
+					XFree (xim_styles);
+				}
+			}
+			if (im && xim_style) 
+			{
+				ic = XCreateIC (im, XNInputStyle, xim_style, 
+				XNClientWindow, win,
+					XNFocusWindow, win,
+					NULL);
+				if (ic == NULL)
+					printf ("XCreateIC failed\n");
+			}
+			if (ic)
+				Xutf8LookupString(ic, (XKeyEvent*)event, keys_return, 6, &keysym, 0);
+			else
+				XLookupString((XKeyEvent*)event, keys_return, 6, &keysym, 0);
+#else
+			XLookupString((XKeyEvent*)event, keys_return, 6, &keysym, 0);
+#endif
 // printf("BC_WindowBase::dispatch_event 2 %llx\n", 
 // event->xkey.state);
 // block out control keys
 			if(keysym > 0xffe0 && keysym < 0xffff) break;
 
-
-			if(test_keypress) printf("BC_WindowBase::dispatch_event %lx\n", keysym);
-
-
-  			switch(keysym)
+			if(test_keypress) printf("BC_WindowBase::dispatch_event %lx\n", (unsigned char)keysym);
+#ifdef X_HAVE_UTF8_STRING
+			//It's Ascii or UTF8?
+			if ( (keys_return[0] & 0xff) >= 0x7f ) {
+				key_pressed_utf8 = keys_return;
+				key_pressed = keysym & 0xff;
+			}
+			else
 			{
+#endif
+				switch(keysym)
+				{
 // block out extra keys
-        		case XK_Alt_L:      
+			case XK_Alt_L:
         		case XK_Alt_R:      
         		case XK_Shift_L:    
         		case XK_Shift_R:    
@@ -950,11 +1007,17 @@ int BC_WindowBase::dispatch_event()
 				case XK_KP_Insert:      key_pressed = KPINS;     break;
 				case XK_KP_Decimal:
 				case XK_KP_Delete:      key_pressed = KPDEL;     break;
- 	    		default:           
-					//key_pressed = keys_return[0]; 
+ 	    		default:        
+#ifdef X_HAVE_UTF8_STRING
+					keys_return[1] = 0;
+					key_pressed_utf8 = keys_return;
 					key_pressed = keysym & 0xff;
+#else
+					key_pressed = keysym & 0xff;
+#endif
 					break;
 			}
+		}
 
 //printf("BC_WindowBase::dispatch_event %d %d %x\n", shift_down(), alt_down(), key_pressed);
 			result = dispatch_keypress_event();
@@ -1862,39 +1925,45 @@ int BC_WindowBase::init_fonts()
 void BC_WindowBase::init_xft()
 {
 #ifdef HAVE_XFT
-	if(!(largefont_xft = XftFontOpenXlfd(display,
-		screen,
-		resources.large_font_xft)))
-		if(!(largefont_xft = XftFontOpenXlfd(display,
-			screen,
-			resources.large_font_xft2)))
-			largefont_xft = XftFontOpenXlfd(display,
-		    	screen,
-		    	"fixed");
+// Rewrite to be fonts chooser ready //akirad
+	if(resources.large_font_xft[0] == '-')
+	{
+		largefont_xft = XftFontOpenXlfd(display,
+						screen,
+						resources.large_font_xft);
+	}
+	else
+	{
+		largefont_xft = XftFontOpenName(display,
+						screen,
+						resources.large_font_xft);
+	}
 
+	if(resources.medium_font_xft[0] == '-')
+	{
+		mediumfont_xft = XftFontOpenXlfd(display,
+						screen,
+						resources.medium_font_xft);
+	}
+	else
+	{
+		mediumfont_xft = XftFontOpenName(display,
+						screen,
+						resources.medium_font_xft);
+	}
 
-	if(!(mediumfont_xft = XftFontOpenXlfd(display,
-		  screen,
-		  resources.medium_font_xft)))
-		if(!(mediumfont_xft = XftFontOpenXlfd(display,
-			  screen,
-			  resources.medium_font_xft2)))
-			mediumfont_xft = XftFontOpenXlfd(display,
-		    	screen,
-		    	"fixed");
-
-
-	if(!(smallfont_xft = XftFontOpenXlfd(display,
-	      screen,
-	      resources.small_font_xft)))
-		if(!(smallfont_xft = XftFontOpenXlfd(display,
-	    	  screen,
-	    	  resources.small_font_xft2)))
-			  smallfont_xft = XftFontOpenXlfd(display,
-		    	  screen,
-		    	  "fixed");
-
-
+	if(resources.small_font_xft[0] == '-')
+	{
+		smallfont_xft = XftFontOpenXlfd(display,
+						screen,
+						resources.small_font_xft);
+	}
+	else
+	{
+		smallfont_xft = XftFontOpenName(display,
+						screen,
+						resources.small_font_xft);
+	}
 // Extension failed to locate fonts
 	if(!largefont_xft || !mediumfont_xft || !smallfont_xft)
 	{
@@ -2281,9 +2350,13 @@ int BC_WindowBase::get_single_text_width(int font, char *text, int length)
 	if(get_resources()->use_xft && get_xft_struct(font))
 	{
 		XGlyphInfo extents;
+#ifdef X_HAVE_UTF8_STRING
+		XftTextExtentsUtf8(top_level->display,
+#else
 		XftTextExtents8(top_level->display,
+#endif
 			get_xft_struct(font),
-			(FcChar8*)text, 
+			(const XftChar8 *)text,
 			length,
 			&extents);
 		return extents.xOff;
@@ -2346,9 +2419,13 @@ int BC_WindowBase::get_text_ascent(int font)
 	if(get_resources()->use_xft && get_xft_struct(font))
 	{
 		XGlyphInfo extents;
+#ifdef X_HAVE_UTF8_STRING
+		XftTextExtentsUtf8(top_level->display,
+#else
 		XftTextExtents8(top_level->display,
+#endif
 			get_xft_struct(font),
-			(FcChar8*)"O", 
+			(const XftChar8 *)"O",
 			1,
 			&extents);
 		return extents.y + 2;
@@ -2383,9 +2460,13 @@ int BC_WindowBase::get_text_descent(int font)
 	if(get_resources()->use_xft && get_xft_struct(font))
 	{
 		XGlyphInfo extents;
+#ifdef X_HAVE_UTF8_STRING
+		XftTextExtentsUtf8(top_level->display,
+#else
 		XftTextExtents8(top_level->display,
+#endif
 			get_xft_struct(font),
-			(FcChar8*)"j", 
+			(const XftChar8 *)"j",
 			1,
 			&extents);
 		return extents.height - extents.y;
@@ -3167,7 +3248,12 @@ int BC_WindowBase::ctrl_down()
 {
 	return top_level->ctrl_mask;
 }
-
+#ifdef X_HAVE_UTF8_STRING
+char* BC_WindowBase::get_keypress_utf8()
+{
+	return top_level->key_pressed_utf8;
+}
+#endif
 
 int BC_WindowBase::get_keypress()
 {

cinelerra/main.C
@@ -78,31 +78,6 @@ int main(int argc, char *argv[])
 	Garbage::garbage = new Garbage;
 	EDL::id_lock = new Mutex("EDL::id_lock");
 
-// detect an UTF-8 locale and try to use a non-Unicode locale instead
-// <---Beginning of dirty hack
-// This hack will be removed as soon as Cinelerra is UTF-8 compliant
-    char *s, *language;
-
-// Query user locale
-    if ((s = getenv("LC_ALL"))  || 
-		(s = getenv("LC_MESSAGES")) || 
-		(s = getenv("LC_CTYPE")) || 
-		(s = getenv ("LANG")))
-    {
-// Test if user locale is set to Unicode        
-        if (strstr(s, ".UTF-8"))
-        {
-// extract language  from language-charset@variant
-          language = strtok (s, ".@");
-// set language as the default locale
-          setenv("LANG", language, 1);
-        }
-    }
-// End of dirty hack --->
-
-
-
-
 	bindtextdomain (PACKAGE, LOCALEDIR);
 	textdomain (PACKAGE);
 	setlocale (LC_MESSAGES, "");

configure.ac
@@ -109,6 +109,51 @@ if test "$no_x" = yes; then
 fi
 AH_TEMPLATE(HAVE_LIBXXF86VM, [Define to 1 if libXxf86vm is available.])
 AC_DEFINE(HAVE_LIBXXF86VM)
+############# XFT
+AC_MSG_CHECKING([whether to use xft])
+AC_ARG_ENABLE(xft, AC_HELP_STRING([--enable-xft],
+	[use freetype/fontconfig/xft (default: on)]),
+	[enable_xft=$enableval], [enable_xft="default"])
+XFT_CFLAGS=""
+XFT_LIBS=""
+if test "$enable_xft" = "no" ; then
+	AC_MSG_RESULT([$enable_xft])
+	XFT_CFLAGS=`pkg-config xft --cflags 2>/dev/null` || found_xft="no"
+	XFT_LIBS=`pkg-config xft --libs 2>/dev/null` || found_xft="no"
+	CXXFLAGS="$XFT_CFLAGS $CXXFLAGS"
+else
+	found_xft="yes"
+	dnl make sure package configurator (pkg-config xft or pkg-config
+	dnl says that xft is present.
+	XFT_CFLAGS=`pkg-config xft --cflags 2>/dev/null` || found_xft="no"
+	XFT_LIBS=`pkg-config xft --libs 2>/dev/null` || found_xft="no"
+	if ! test "$found_xft" = "no" ; then
+		found_xft=yes
+	fi
+	AC_MSG_RESULT([$found_xft])
+	dnl print a warning if xft is unusable and was specifically requested
+	if test "$found_xft" = "no" ; then
+		if test "$enable_xft" = "yes" ; then
+			AC_MSG_WARN([Can't find xft configuration, or xft is unusable])
+		fi
+		enable_xft=no
+		XFT_CFLAGS=""
+		XFT_LIBS=""
+	else
+		enable_xft=yes
+		## To force to all project - uncomment
+		##X_EXTRA_LIBS="$XFT_LIBS $X_EXTRA_LIBS"
+		CXXFLAGS="$XFT_CFLAGS $CXXFLAGS"
+		AH_TEMPLATE(HAVE_XFT, [Define to 1 if libXft is available.])
+		AC_DEFINE(HAVE_XFT)
+		## need some fix to compile with XFT
+		AC_DEFINE(
+			[PNG_SETJMP_NOT_SUPPORTED], [1],
+			[Workaround to fix compile with xft])
+	fi
+fi
+AC_SUBST(XFT_CFLAGS)
+AC_SUBST(XFT_LIBS)
 ############## END XLIB
 
 ############## SOUND

guicast/Makefile.am
@@ -1,7 +1,7 @@
 lib_LTLIBRARIES = libguicast.la
 noinst_PROGRAMS = bootstrap pngtoh
 
-libguicast_la_LIBADD = $(OPENGL_LIBS) -lXxf86vm -lXv -lXext -lX11 -lpng $(X_EXTRA_LIBS)
+libguicast_la_LIBADD = $(OPENGL_LIBS) -lXxf86vm -lXv -lXext -lX11 -lpng $(X_EXTRA_LIBS) $(XFT_LIBS)
 libguicast_la_LDFLAGS = $(X_LIBS) -version-info 1:0:0 
 libguicast_la_SOURCES = \
 	bcbar.C \

guicast/bcresources.C
@@ -68,15 +68,15 @@ const char* BC_Resources::medium_font2 = N_("-*-helvetica-bold-r-normal-*-14-*")
 const char* BC_Resources::large_font = N_("-*-helvetica-bold-r-normal-*-18-*");
 const char* BC_Resources::large_font2 = N_("-*-helvetica-bold-r-normal-*-20-*");
 
-const char* BC_Resources::small_fontset = "6x12,*";
-const char* BC_Resources::medium_fontset = "7x14,*";
-const char* BC_Resources::large_fontset = "8x16,*";
+const char* BC_Resources::small_fontset = "-*-helvetica-medium-r-normal-*-10-*";
+const char* BC_Resources::medium_fontset = "-*-helvetica-bold-r-normal-*-14-*";
+const char* BC_Resources::large_fontset = "-*-helvetica-bold-r-normal-*-18-*";
 
-const char* BC_Resources::small_font_xft = N_("-*-luxi sans-*-r-*-*-12-*-*-*-*-*-*-*");
+const char* BC_Resources::small_font_xft = N_("Sans-8");
 const char* BC_Resources::small_font_xft2 = N_("-microsoft-verdana-*-*-*-*-*-*-*-*-*-*-*-*");
-const char* BC_Resources::medium_font_xft = N_("-*-luxi sans-*-r-*-*-16-*-*-*-*-*-*-*");
+const char* BC_Resources::medium_font_xft = N_("Sans-10");
 const char* BC_Resources::medium_font_xft2 = N_("-microsoft-verdana-*-*-*-*-*-*-*-*-*-*-*-*");
-const char* BC_Resources::large_font_xft = N_("-*-luxi sans-bold-r-*-*-20-*-*-*-*-*-*-*");
+const char* BC_Resources::large_font_xft = N_("Sans-16");
 const char* BC_Resources::large_font_xft2 = N_("-microsoft-verdana-*-*-*-*-*-*-*-*-*-*-*-*");
 
 suffix_to_type_t BC_Resources::suffix_to_type[] = 
@@ -526,7 +526,7 @@ BC_Resources::BC_Resources()
 
 	audiovideo_color = RED;
 
-	use_fontset = 0;
+	use_fontset = 1;
 
 // Xft has priority over font set
 #ifdef HAVE_XFT

guicast/bcwindowbase.h
@@ -173,6 +173,11 @@ public:
 	friend class BC_Window;
 	friend class BC_WindowEvents;
 
+#ifdef X_HAVE_UTF8_STRING
+	XIM im;         /* Used to communicate with the input method (IM) server */
+	XIC ic;         /* Used for retaining the state, properties, and semantics of communication with                                                  the input method (IM) server */
+#endif
+
 // Main loop
 	int run_window();
 // Terminal event dispatchers
@@ -275,6 +280,9 @@ public:
 	int get_buttonpress();
 	int get_has_focus();
 	int get_dragging();
+#ifdef X_HAVE_UTF8_STRING
+	char* get_keypress_utf8();
+#endif
 	int get_keypress();
 // Get cursor position of last event
 	int get_cursor_x();
@@ -719,6 +727,10 @@ private:
 // Which button is down.  1, 2, 3, 4, 5
 	int button_pressed;
 // Last key pressed
+	
+#ifdef X_HAVE_UTF8_STRING
+	char* key_pressed_utf8;
+#endif
 	int key_pressed;
 // During a selection drag involving toggles, set the same value for each toggle
 	int toggle_value;

guicast/bcwindowdraw.C
@@ -302,20 +302,20 @@ void BC_WindowBase::draw_xft_text(int x,
 // top_level->get_xft_struct(top_level->current_font),
 // x2 + k, 
 // y2 + k,
-// (FcChar8*)&text[j],
+// (const XftChar8 *)&text[j],
 // i - j);
+#ifdef X_HAVE_UTF8_STRING
+	XftDrawStringUtf8 (
+#else
 	XftDrawString8 (
+#endif
 		(XftDraw*)(pixmap ? pixmap->opaque_xft_draw : this->pixmap->opaque_xft_draw),
 		&xft_color,
 		top_level->get_xft_struct(top_level->current_font),
 		x2 + k, 
 		y2 + k,
-		(FcChar8*)&text[j],
+		(const FcChar8 *)&text[j],
 		i - j);
-	XftColorFree(top_level->display,
-	    top_level->vis,
-	    top_level->cmap,
-	    &xft_color);
 #endif
 }
 

commit 2e8985839fa0f5abe47bf82a79e4540e1ea01371

Fixed possible buffer overflow in BC_WindowBase::dispatch_event
Bug was introduced in commit 6d6012573eb8176d76329a7fbcc847b66879e587

guicast/bcwindowbase.C
@@ -936,7 +936,7 @@ int BC_WindowBase::dispatch_event()
 			else
 				XLookupString((XKeyEvent*)event, keys_return, 6, &keysym, 0);
 #else
-			XLookupString((XKeyEvent*)event, keys_return, 6, &keysym, 0);
+			XLookupString((XKeyEvent*)event, keys_return, 2, &keysym, 0);
 #endif
 // printf("BC_WindowBase::dispatch_event 2 %llx\n", 
 // event->xkey.state);