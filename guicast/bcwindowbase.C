e939d3d4fb3e39ddb6ffe428b38f55460c70bcc5
Einar Rünkaru <einarry@smail.ee>
2014-07-04 13:50:41 +0300

    Use xft font parameters to get the height of row

--- guicast/bcwindowbase.C
+++ guicast/bcwindowbase.C
@@ -2418,29 +2418,10 @@ int BC_WindowBase::get_text_width(int font, const char *text, int length)
 int BC_WindowBase::get_text_ascent(int font)
 {
 #ifdef HAVE_XFT
-	if(get_resources()->use_xft && get_xft_struct(font))
-	{
-		XGlyphInfo extents;
-#ifdef X_HAVE_UTF8_STRING
-		if(get_resources()->locale_utf8)
-		{
-			XftTextExtentsUtf8(top_level->display,
-			get_xft_struct(font),
-			(const XftChar8 *)"O",
-			1,
-			&extents);
-		}
-		else
-#endif
-		{
-			XftTextExtents8(top_level->display,
-				get_xft_struct(font),
-				(const XftChar8 *)"O",
-				1,
-				&extents);
-		}
-		return extents.y + 2;
-	}
+	XftFont *fstruct;
+
+	if(get_resources()->use_xft && (fstruct = get_xft_struct(font)))
+		return fstruct->ascent;
 	else
 #endif
 	if(get_resources()->use_fontset && top_level->get_fontset(font))
@@ -2468,29 +2449,10 @@ int BC_WindowBase::get_text_ascent(int font)
 int BC_WindowBase::get_text_descent(int font)
 {
 #ifdef HAVE_XFT
-	if(get_resources()->use_xft && get_xft_struct(font))
-	{
-		XGlyphInfo extents;
-#ifdef X_HAVE_UTF8_STRING
-		if(get_resources()->locale_utf8)
-		{
-			XftTextExtentsUtf8(top_level->display,
-				get_xft_struct(font),
-				(const XftChar8 *)"j",
-				1,
-				&extents);
-		}
-		else
-#endif
-		{
-			XftTextExtents8(top_level->display,
-				get_xft_struct(font),
-				(const XftChar8 *)"j",
-				1,
-				&extents);
-		}
-		return extents.height - extents.y;
-	}
+	XftFont *fstruct;
+
+	if(get_resources()->use_xft && (fstruct = get_xft_struct(font)))
+		return fstruct->descent;
 	else
 #endif
     if(get_resources()->use_fontset && top_level->get_fontset(font))
@@ -2514,7 +2476,17 @@ int BC_WindowBase::get_text_descent(int font)
 
 int BC_WindowBase::get_text_height(int font, const char *text)
 {
-	if(!text) return get_text_ascent(font) + get_text_descent(font);
+	int rowh;
+#ifdef HAVE_XFT
+	XftFont *fstruct;
+
+	if(get_resources()->use_xft && (fstruct = get_xft_struct(font)))
+		rowh = fstruct->height;
+	else
+#endif
+		rowh = get_text_ascent(font) + get_text_descent(font);
+
+	if(!text) return rowh;
 
 // Add height of lines
 	int h = 0, i, length = strlen(text);
@@ -2526,7 +2498,7 @@ int BC_WindowBase::get_text_height(int font, const char *text)
 		if(text[i] == 0)
 			h++;
 	}
-	return h * (get_text_ascent(font) + get_text_descent(font));
+	return h * rowh;
 }
 
 BC_Bitmap* BC_WindowBase::new_bitmap(int w, int h, int color_model)
