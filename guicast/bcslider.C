cinelerra-cv-team/cinelerra-cv

commit 6d2cf4b21fa1e7f7018c61b5059480d872308fc3

Make tooltip to accept utf8 besides locale encoding
Keep tooltip internally as wchar_t text.

guicast/bcslider.C
@@ -30,9 +30,8 @@
 
 
 
-#include <ctype.h>
 #include <string.h>
-
+#include <wctype.h>
 
 
 BC_Slider::BC_Slider(int x, 
@@ -199,7 +198,7 @@ int BC_Slider::repeat_event(int64_t duration)
 		else
 		if(status == SLIDER_HI)
 		{
-			if(!tooltip_text[0] || isdigit(tooltip_text[0]))
+			if(tooltip_wtext || iswdigit(tooltip_wtext[0]))
 			{
 				set_tooltip(get_caption());
 				show_tooltip(50);

guicast/bcbutton.C
@@ -156,7 +156,7 @@ int BC_Button::draw_face()
 int BC_Button::repeat_event(int64_t duration)
 {
 	if(duration == top_level->get_resources()->tooltip_delay &&
-		tooltip_text[0] != 0 &&
+		tooltip_wtext &&
 		status == BUTTON_UPHI &&
 		!tooltip_done)
 	{

guicast/bclistbox.C
@@ -2405,7 +2405,7 @@ int BC_ListBox::repeat_event(int64_t duration)
 // Show tooltip
 			if(button_highlighted &&
 				duration == get_resources()->tooltip_delay &&
-				tooltip_text[0] != 0 &&
+				tooltip_wtext &&
 				is_popup &&
 				!tooltip_done)
 			{

guicast/bcpan.C
@@ -186,7 +186,7 @@ int BC_Pan::button_release_event()
 int BC_Pan::repeat_event(int64_t duration)
 {
 	if(duration == top_level->get_resources()->tooltip_delay &&
-		tooltip_text[0] != 0 &&
+		tooltip_wtext &&
 		highlighted &&
 		!active &&
 		!tooltip_done)

guicast/bcpopupmenu.C
@@ -336,7 +336,7 @@ int BC_PopupMenu::focus_out_event()
 int BC_PopupMenu::repeat_event(int64_t duration)
 {
 	if(duration == top_level->get_resources()->tooltip_delay &&
-		tooltip_text[0] != 0 &&
+		tooltip_wtext &&
 		status == BUTTON_HI &&
 		!tooltip_done)
 	{

guicast/bcpot.C
@@ -25,8 +25,7 @@
 #include "keys.h"
 #include "units.h"
 #include "vframe.h"
-
-#include <ctype.h>
+#include <wctype.h>
 #include <math.h>
 #include <string.h>
 #define MIN_ANGLE 225
@@ -220,7 +219,7 @@ int BC_Pot::repeat_event(int64_t duration)
 		{
 			if(use_caption)
 			{
-				if(!tooltip_text[0] || isdigit(tooltip_text[0]))
+				if(!tooltip_wtext || iswdigit(tooltip_wtext[0]))
 				{
 					set_tooltip(get_caption());
 					show_tooltip(50);

guicast/bctextbox.C
@@ -749,7 +749,7 @@ int BC_TextBox::repeat_event(int64_t duration)
 	int result = 0;
 
 	if(duration == top_level->get_resources()->tooltip_delay &&
-		tooltip_text[0] != 0 &&
+		tooltip_wtext &&
 		highlighted)
 	{
 		show_tooltip();

guicast/bctoggle.C
@@ -265,7 +265,7 @@ void BC_Toggle::set_status(int value)
 int BC_Toggle::repeat_event(int64_t duration)
 {
 	if(duration == top_level->get_resources()->tooltip_delay &&
-		tooltip_text[0] != 0 &&
+		tooltip_wtext &&
 		(status == BC_Toggle::TOGGLE_UPHI || status == BC_Toggle::TOGGLE_CHECKEDHI) &&
 		!tooltip_done)
 	{

commit d632bb30d41053b929cf754a299f82e532d4db3e

Fixed crash when activating tooltip in BC_Slider::repeat_event
Bug was introduced in commit 6d2cf4b21

guicast/bcslider.C
@@ -198,7 +198,7 @@ int BC_Slider::repeat_event(int64_t duration)
 		else
 		if(status == SLIDER_HI)
 		{
-			if(tooltip_wtext || iswdigit(tooltip_wtext[0]))
+			if(!tooltip_wtext || iswdigit(tooltip_wtext[0]))
 			{
 				set_tooltip(get_caption());
 				show_tooltip(50);