cinelerra-cv-team/cinelerra-cv

commit 9d1bcae1aad98a3f54dde6ac727e3fe4acb0b799

Quicker initialization of empty strings (s[0]=0; instead of sprintf(s,""); )
Use s[0]=0 instead of sprintf(s,) to null terminate an empty string.
Get rid of warning: zero-length gnu_printf format string [-Wformat-zero-length].

guicast/filesystem.C
@@ -700,7 +700,7 @@ int FileSystem::extract_name(char *out, const char *in, int test_dir)
 	int i;
 
 	if(test_dir && is_dir(in))
-		sprintf(out, "");    // complete string is directory
+		string[0] = 0;    // complete string is directory
 	else
 	{
 		for(i = strlen(in)-1; i > 0 && in[i] != '/'; i--)

cinelerra/audioesound.C
@@ -84,7 +84,7 @@ char* AudioESound::translate_device_string(char *server, int port)
 	if(port > 0 && strlen(server))
 		sprintf(device_string, "%s:%d", server, port);
 	else
-		sprintf(device_string, "");
+		device_string[0] = 0;
 	return device_string;
 }
 

cinelerra/menueffects.C
@@ -92,7 +92,7 @@ MenuEffectPacket::~MenuEffectPacket()
 MenuEffectThread::MenuEffectThread(MWindow *mwindow)
 {
 	this->mwindow = mwindow;
-	sprintf(title, "");
+	title[0] = 0;
 }
 
 MenuEffectThread::~MenuEffectThread()

cinelerra/playbackconfig.C
@@ -45,7 +45,7 @@ AudioOutConfig::AudioOutConfig(int duplex)
 		sprintf(oss_out_device[i], "/dev/dsp");
 	}
 
-	sprintf(esound_out_server, "");
+	esound_out_server[0] = 0;
 	esound_out_port = 0;
 
 	sprintf(alsa_out_device, "default");
@@ -264,7 +264,7 @@ VideoOutConfig::VideoOutConfig()
 	driver = PLAYBACK_X11_XV;
 	buz_out_channel = 0;
 	buz_swap_fields = 0;
-	sprintf(x11_host, "");
+	x11_host[0] = 0;
 	x11_use_fields = USE_NO_FIELDS;
 
 	firewire_channel = 63;

cinelerra/recordconfig.C
@@ -46,7 +46,7 @@ AudioInConfig::AudioInConfig()
 	firewire_port = 0;
 	firewire_channel = 63;
 	strcpy(firewire_path, "/dev/dv1394");
-	sprintf(esound_in_server, "");
+	esound_in_server[0] = 0;
 	esound_in_port = 0;
 
 	sprintf(alsa_in_device, "default");
@@ -184,7 +184,7 @@ VideoInConfig::VideoInConfig()
 	sprintf(v4l2jpeg_in_device, "/dev/video0");
 	sprintf(lml_in_device, "/dev/mvideo/stream");
 	sprintf(buz_in_device, "/dev/video0");
-	sprintf(screencapture_display, "");
+	screencapture_display[0] = 0;
 
 
 // DVB

cinelerra/tracks.C
@@ -191,7 +191,7 @@ int Tracks::load(FileXML *xml, int &track_offset, uint32_t load_flags)
 // add the appropriate type of track
 	char string[BCTEXTLEN];
 	Track *track = 0;
-	sprintf(string, "");
+	string[0] = 0;
 	
 	xml->tag.get_property("TYPE", string);
 

cinelerra/tracksedit.C
@@ -682,7 +682,7 @@ void Tracks::paste_automation(double selectionstart,
 	double frame_rate = edl->session->frame_rate;
 	int64_t sample_rate = edl->session->sample_rate;
 	char string[BCTEXTLEN];
-	sprintf(string, "");
+	string[0] = 0;
 
 // Search for start
 	do{

cinelerra/vdeviceprefs.C
@@ -519,7 +519,7 @@ char* VDriverMenu::driver_to_string(int driver)
 			break;
 #endif
 		default:
-			sprintf(string, "");
+			string[0] = 0;
 	}
 	return string;
 }

commit b089d2837100ec071ac2544789a950752c335869

Fixed clearing of wrong string in FileSystem::extract_name
File->save gave garbage in the place of file name
Bug introduced in commit 9d1bcae1a

Reported-by: Igor Vladimirsky <sitelve@gmail.com>

guicast/filesystem.C
@@ -700,7 +700,7 @@ int FileSystem::extract_name(char *out, const char *in, int test_dir)
 	int i;
 
 	if(test_dir && is_dir(in))
-		string[0] = 0;    // complete string is directory
+		out[0] = 0;    // complete string is directory
 	else
 	{
 		for(i = strlen(in)-1; i > 0 && in[i] != '/'; i--)