bfa714c8aa80ea4b9f3682f95dc9f97e16546040
Einar Rünkaru <einarrunkaru@gmail.com>
2017-08-04 18:49:45 +0300

    Removed disabled code from Sema

--- guicast/sema.C
+++ guicast/sema.C
@@ -22,9 +22,6 @@
 #include "bcsignals.h"
 #include "sema.h"
 
-
-
-
 Sema::Sema(int init_value, const char *title)
 {
 	sem_init(&sem, 0, init_value);
@@ -37,7 +34,6 @@ Sema::~Sema()
 	sem_destroy(&sem);
 	UNSET_ALL_LOCKS(this);
 }
-
 
 void Sema::lock(const char *location)
 {
@@ -65,91 +61,3 @@ void Sema::reset()
 	sem_init(&sem, 0, init_value);
 	UNSET_ALL_LOCKS(this)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-#include "bcipc.h"
-#include "sema.h"
-
-Sema::Sema(int id, int number)
-{
-	if(id == -1)
-	{
-		if((semid = semget(IPC_PRIVATE, number, IPC_CREAT | 0777)) < 0) perror("Sema::Sema");
-		for(int i = 0; i < number; i++) unlock(i);
-		client = 0;
-		bc_enter_sema_id(semid);
-	}
-	else
-	{
-		client = 1;
-		this->semid = id;
-	}
-
-	semas = number;
-}
-
-Sema::~Sema()
-{
-	if(!client)
-	{
-		if(semctl(semid, 0, IPC_RMID, arg) < 0) perror("Sema::~Sema");
-		bc_remove_sema_id(semid);
-	}
-}
-
-	
-int Sema::lock(int number)
-{
-	struct sembuf sop;
-	
-// decrease the semaphore
-	sop.sem_num = number;
-	sop.sem_op = -1;
-	sop.sem_flg = 0;
-	if(semop(semid, &sop, 1) < 0) perror("Sema::lock");
-	return 0;
-}
-
-int Sema::unlock(int number)
-{
-	struct sembuf sop;
-	
-// decrease the semaphore
-	sop.sem_num = number;
-	sop.sem_op = 1;
-	sop.sem_flg = 0;
-	if(semop(semid, &sop, 1) < 0) perror("Sema::unlock");
-	return 0;
-}
-
-int Sema::get_value(int number)
-{
-	return semctl(semid, number, GETVAL, arg);
-}
-
-int Sema::get_id()
-{
-	return semid;
-}
-
-
-#endif
--- guicast/sema.h
+++ guicast/sema.h
@@ -41,49 +41,4 @@ public:
 	int init_value;
 };
 
-
-
-
-
-
-
-
-#if 0
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/types.h>
-
-union semun {
-   int val; /* used for SETVAL only */
-   struct semid_ds *buf; /* for IPC_STAT and IPC_SET */
-   ushort *array;  /* used for GETALL and SETALL */
-};
-
-class Sema
-{
-public:
-	Sema(int id = -1, int number = 1);
-	~Sema();
-
-	int lock(int number = 0);
-	int unlock(int number = 0);
-	int get_value(int number = 0);
-	int get_id();
-
-	int semid;
-	int client;
-	int semas;
-	semun arg;
-};
-
-#endif
-
-
-
-
-
 #endif
