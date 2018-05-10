cinelerra-cv-team/cinelerra-cv

commit 2379afddfada0c353b3e8398772db6c1bb453e21

Fix markup of translatable string in mwindow.C
Fixed untranslatable string errormsg.

cinelerra/mwindow.C
@@ -907,7 +907,7 @@ SET_TRACE
 					(new_asset->height % 2)))
 				{
 					char string[BCTEXTLEN];
-					sprintf(string, "%s's resolution is %dx%d.\nImages with odd dimensions may not decode properly.",
+					sprintf(string, (_("%s's resolution is %dx%d.\nImages with odd dimensions may not decode properly."),
 						new_asset->path,
 						new_asset->width,
 						new_asset->height);

commit 978376b7de3fd2036dd6bbce2c62afaf868e0069

Fixed compilation error in mwindow.C
Error was introduced in commit 2379afdd

cinelerra/mwindow.C
@@ -907,7 +907,7 @@ SET_TRACE
 					(new_asset->height % 2)))
 				{
 					char string[BCTEXTLEN];
-					sprintf(string, (_("%s's resolution is %dx%d.\nImages with odd dimensions may not decode properly."),
+					sprintf(string, _("%s's resolution is %dx%d.\nImages with odd dimensions may not decode properly."),
 						new_asset->path,
 						new_asset->width,
 						new_asset->height);